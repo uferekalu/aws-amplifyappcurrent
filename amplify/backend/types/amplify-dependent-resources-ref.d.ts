71b178e13f14889d407da9682204eca57dc81839
Ufere Kalu <dekalusha@gmail.com>
2021-12-24 16:04:03 -0600

    added auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "awsamplifyappcurrent5bd1c016": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Amplify from 'aws-amplify';
+import awsmobile from './aws-exports';
+Amplify.configure(awsmobile);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
